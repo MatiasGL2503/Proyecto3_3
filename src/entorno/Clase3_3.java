e0abcf454bb1ab9c16c43916ea5e6914cea1dcb2
MatiasGL2503 <matiazn92@gmail.com>
2024-10-24 17:40:39 +0200

    asd

--- src/entorno/Clase3_3.java
+++ src/entorno/Clase3_3.java
@@ -3,7 +3,7 @@ package entorno;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Practica3_3 Matias Gomez");
+		System.out.println("Practica3_3 Matias Gomez maricona");
 
 	}
 
