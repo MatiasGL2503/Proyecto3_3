664c63688012a2272619026069285380b0508181
MatiasGL2503 <matiazn92@gmail.com>
2024-10-24 17:50:13 +0200

    He editado clase3

--- src/entorno/Clase3_3.java
+++ src/entorno/Clase3_3.java
@@ -3,7 +3,8 @@ package entorno;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Practica3_3 Matias Gomez maricona");
+		System.out.println("CanelaEnRama");
+		
 
 	}
 
