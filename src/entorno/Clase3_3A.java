7e1b769f234019a59a5bd4534cc1a5b5b8e5de16
MatiasGL2503 <matiazn92@gmail.com>
2024-10-24 16:48:48 +0200

    Primer submit

---
+++ src/entorno/Clase3_3A.java
@@ -0,0 +1,17 @@
+package entorno;
+
+import java.util.Scanner;
+
+public class Clase3_3A {
+
+	public static void main(String[] args) {
+		Scanner p = new Scanner(System.in);
+		String texto;
+		System.out.println("Cual es tu nombre");
+		texto = p.nextLine();
+		System.out.println("vale, crack");
+		p.close();
+
+	}
+
+}
